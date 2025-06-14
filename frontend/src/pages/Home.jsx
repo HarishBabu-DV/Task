a111b747378abc0a53998bfe70bf749ba963bbbd
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-06-15 00:07:11 +0530

    created posts page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,15 +3,15 @@ import Button from "../components/Button"
 
 const Home = () => {
   return (
-    <section className="hero-section">
-        <h1 className="text-center text-[5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name">Beleaf Technologies</h1>
+    <section className="hero-section gradient-background">
+        <h1 className="text-center text-[5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name ">Beleaf Technologies</h1>
         <h1 className="text-center text-5xl max-md:text-4xl max-sm:text-2xl font-bold">Full Stack Development Task</h1>
         <ul className="ml-8 list-disc text-[1.2rem] max-md:text-lg max-sm:text-[1rem] ">
             <li> To implement CRUD Operations</li>
             <li> To Fetch data from JsonPlaceholder</li>
             <li> To use React for FrontEnd, Node.js for BackEnd, MongoDB for DataBase</li>
         </ul>
-       <Link className={'view-post-button blue-gradient'}>
+       <Link to={'/posts'} className={'view-post-button blue-gradient'}>
             View Posts
        </Link>
 
--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
 const Posts = () => {
+  const [posts,setPosts]=useState([])
+  const tableHeadings=posts?.map(post=>Object.keys(post))
+  const getPosts=async () => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
   return (
-    <div>Posts</div>
+    <section className='gradient-background '>
+      <h1 className="text-center text-[2.5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name">Posts</h1>
+      <table>
+        <thead>
+        
+        </thead>  
+      </table>
+    </section>
   )
 }
 
