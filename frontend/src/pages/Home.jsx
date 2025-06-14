ac5d925fa06d006521a1f2f091848e2e10badf30
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-06-14 23:06:40 +0530

    updated home page with responsiveness

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,19 @@
+import { Link } from "react-router"
 import Button from "../components/Button"
 
 const Home = () => {
   return (
     <section className="hero-section">
-        <h1 className="text-[5rem] font-bold company-name">Beleaf Technologies</h1>
-        <h1 className="text-5xl font-bold">Full Stack Development Task</h1>
-        <ul className="list-disc text-[1.2rem]">
+        <h1 className="text-center text-[5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name">Beleaf Technologies</h1>
+        <h1 className="text-center text-5xl max-md:text-4xl max-sm:text-2xl font-bold">Full Stack Development Task</h1>
+        <ul className="ml-8 list-disc text-[1.2rem] max-md:text-lg max-sm:text-[1rem] ">
             <li> To implement CRUD Operations</li>
             <li> To Fetch data from JsonPlaceholder</li>
             <li> To use React for FrontEnd, Node.js for BackEnd, MongoDB for DataBase</li>
         </ul>
-        
-        <Button className={'view-post-button blue-gradient'}>
+       <Link className={'view-post-button blue-gradient'}>
             View Posts
-        </Button>
-       
+       </Link>
 
     </section>
   )
