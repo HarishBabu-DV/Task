24d6473a80991a827dbe2b98381b699b530ecf17
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-06-15 12:22:24 +0530

    modified package.json file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const postsRoutes=require('./routes/postsRoutes')
 connectToDB()
 
 const app=express()
-const PORT=process.env.PORT 
+const PORT=process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/api/v1/home',homeRoutes)
