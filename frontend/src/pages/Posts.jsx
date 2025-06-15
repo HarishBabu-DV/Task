ee8e7d7560a7adbe130ca4ff6730d3a795b6d2e1
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-06-15 12:17:13 +0530

    completed retrieval of data from jsonplaceholder

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 const Posts = () => {
   const [posts,setPosts]=useState([])
-  const tableHeadings=posts?.map(post=>Object.keys(post))
+  //Initially used jsonplaceholder api to fetch data
   const getPosts=async () => {
     try {
-      
+      const response=await axios.get('https://jsonplaceholder.typicode.com/posts')
+      if(response){
+        console.log('posts received');
+        setPosts(response?.data)
+      }
     } catch (error) {
-      
+      console.log(error);
     }
   }
+  console.log(posts);
+  const tableHeadings=Object.keys(posts[0])
+  console.log(tableHeadings);
+   
+  useEffect(()=>{
+    getPosts()
+  },[])
   return (
     <section className='gradient-background '>
       <h1 className="text-center text-[2.5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name">Posts</h1>
       <table>
         <thead>
-        
+        {
+         tableHeadings?.map(tableHeading=>(
+          <th>{tableHeading}</th>
+         ))
+        }
         </thead>  
+        <tbody>
+          {
+            posts && posts.map(post=>(
+              <tr>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </section>
   )
