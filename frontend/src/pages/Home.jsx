3e31fedb05a8ff8eb3815edf08ba3e82817212c5
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-06-15 12:24:00 +0530

    modified heading in home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Button from "../components/Button"
 const Home = () => {
   return (
     <section className="hero-section gradient-background">
-        <h1 className="text-center text-[5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name ">Beleaf Technologies</h1>
+        <h1 className="text-center text-[5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name ">MERN STACK Development</h1>
         <h1 className="text-center text-5xl max-md:text-4xl max-sm:text-2xl font-bold">Full Stack Development Task</h1>
         <ul className="ml-8 list-disc text-[1.2rem] max-md:text-lg max-sm:text-[1rem] ">
             <li> To implement CRUD Operations</li>
