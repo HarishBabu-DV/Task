7066fa8b60ea914d34633164bebfed7ce56724b5
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-06-15 12:55:17 +0530

    installed cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express=require('express')
+const cors=require('cors')
 const connectToDB=require('./database/db')
 const homeRoutes=require('./routes/homeRoutes')
 const postsRoutes=require('./routes/postsRoutes')
@@ -9,6 +10,7 @@ connectToDB()
 const app=express()
 const PORT=process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/v1/home',homeRoutes)
 app.use('/api/v1/posts',postsRoutes)
