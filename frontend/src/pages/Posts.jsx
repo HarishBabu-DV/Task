4247fff8f44476d3aaa3e8e628a1f5947ab0da38
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-06-15 15:17:00 +0530

    modified logic in posts file.jsx

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,40 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { storePosts } from '../services/api/ApiServices'
+
 const Posts = () => {
+  const [recievedPosts,setRecievedPosts]=useState([])
   const [posts,setPosts]=useState([])
+  const [loading,setLoading]=useState(false)
   //Initially used jsonplaceholder api to fetch data
-  const getPosts=async () => {
+  const receivePosts=async () => {
     try {
+      setLoading(true)
       const response=await axios.get('https://jsonplaceholder.typicode.com/posts')
-      if(response){
+      if(response?.data){
         console.log('posts received');
-        setPosts(response?.data)
+        setRecievedPosts(response?.data)
+        setLoading(false)
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+    }
+  }
+
+  const storeNewPosts=async () => {
+    try {
+      const response=await storePosts(receivePosts)
+      if(response){
+        console.log('posts stored');
+        setPosts(response?.data?.data)
       }
     } catch (error) {
       console.log(error);
     }
   }
-  console.log(posts);
-  const tableHeadings=Object.keys(posts[0])
-  console.log(tableHeadings);
-   
+  
+  // console.log(posts);
+  // const tableHeadings=Object.keys(posts[0])
+  // console.log(tableHeadings);
+  console.log("posts :",posts);
   useEffect(()=>{
-    getPosts()
+    receivePosts()
   },[])
+  
+  console.log(recievedPosts);
   return (
     <section className='gradient-background '>
       <h1 className="text-center text-[2.5rem] font-bold max-md:text-[4rem] max-sm:text-[2.5rem] company-name">Posts</h1>
+      {
+        loading ? <h1>Loading</h1> : null
+      }
       <table>
         <thead>
-        {
+        {/* {
          tableHeadings?.map(tableHeading=>(
           <th>{tableHeading}</th>
          ))
-        }
+        } */}
         </thead>  
         <tbody>
-          {
-            posts && posts.map(post=>(
+          {/* {
+            posts && posts?.map(post=>(
               <tr>
                 <td>{post.userId}</td>
                 <td>{post.id}</td>
@@ -42,9 +67,10 @@ const Posts = () => {
                 <td>{post.body}</td>
               </tr>
             ))
-          }
+          } */}
         </tbody>
       </table>
+      <button onClick={()=>storeNewPosts()} className='bg-black text-white py-1.5 px-4 hover:cursor-pointer'>store posts</button>
     </section>
   )
 }
--- frontend/src/services/api/ApiServices.js
+++ frontend/src/services/api/ApiServices.js
@@ -1,1 +1,31 @@
 import axios from "axios";
+
+const API=axios.create({
+    baseURL:'https://mern-stack-task-xo7j.onrender.com'
+})
+//Store posts retrieved from json placeholder
+const storePosts=async (listOfPosts)=>{
+   return await API.post('/api/v1/posts/store',listOfPosts,{
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+//Retrieve posts
+const retrievePosts=async ()=>{
+   return await API.get('/api/v1/posts');
+}
+//Create Post
+const createPost=async ()=>{
+   return await API.post('/api/v1/posts/add');
+}
+//Update Post
+const updatePost=async ()=>{
+   return await API.put('/api/v1/posts/update/:id');
+}
+//Delete Post
+const deletePost=async ()=>{
+   return await API.put('/api/v1/posts/delete/:id');
+}
+
+export {storePosts,retrievePosts,createPost,updatePost,deletePost}
